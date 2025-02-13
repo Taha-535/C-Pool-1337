2ac27a2053e38cba7e1ad4ba788d47a3ee4ed4ed
Taha-535 <tahaelmoatamid@gmail.com>
2025-02-13 23:56:52 +0100

    ex08

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -10,12 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void ft_put_comb(char comb[], int n, int nb, int i)
+{
+  while (i <= 10 - nb)
+  {
+    comb[n - nb] = i + 48;
+    if (nb == 1)
+    {
+      if (i != n - 1)
+      {
+        write(1, ", ", 2);
+      }
+      write(1, comb, n);
+      i++;
+    }
+    else
+    {
+      ft_put_comb(comb, n, nb - 1, i + 1);
+      i++;
+    }
+  }
+}
+
 void ft_print_combn(int n)
 {
   char comb[n];
-}
 
-void main()
-{
-  ft_print_combn(4);
+  ft_put_comb(comb, n, n, 0);
 }
